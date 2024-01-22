ccc9d392c30b3ed9757189f3ec667462119ede3c
Arkadyuti <arkadyuti.paul@travelopia.com>
2024-01-22 16:17:51 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 3000,
+  },
+};
